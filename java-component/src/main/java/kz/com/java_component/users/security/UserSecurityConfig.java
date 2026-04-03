e270040be12a2fe1e6778fec34a9a9503880d63b
Temirlan A <gankstargamer@gmail.com>
2026-04-03 22:36:00 +0500

    chore: ch9 v1 users-app - jdbc auth

--- java-component/src/main/java/kz/com/java_component/users/security/UserSecurityConfig.java
+++ java-component/src/main/java/kz/com/java_component/users/security/UserSecurityConfig.java
@@ -2,15 +2,20 @@ package kz.com.java_component.users.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class UserSecurityConfig {
@@ -27,7 +32,16 @@ public class UserSecurityConfig {
     }
 
     @Bean
-    public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder,
+                                                 DataSource dataSource) {
         var admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin"))
@@ -46,7 +60,11 @@ public class UserSecurityConfig {
                 .roles("USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(admin, manager, user);
+        var users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(admin);
+        users.createUser(manager);
+        users.createUser(user);
+        return users;
     }
 
     @Bean
