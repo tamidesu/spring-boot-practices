4d9400fb4f9b19d6fb7f3e18a2bf8d500b727c12
Temirlan A <gankstargamer@gmail.com>
2026-04-04 00:03:17 +0500

    chore: ch9 my-retro - 09-oauth2 resource server

---
+++ oauth2-server/src/test/java/kz/com/oauth2_server/Oauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.com.oauth2_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
